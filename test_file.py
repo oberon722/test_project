92b11d752d1b776f7c9d520613f6a62fb0dd64e9
oberon722 <oberon722@ya.ru>
2024-02-11 18:02:39 +0300

    Commit_2 собавлены коменнты к тестовой задаче

--- test_file.py
+++ test_file.py
@@ -1,4 +1,22 @@
+
 # ЭТО ТЕСТ
+
+# 1. мой репозиторий для дз 10 семинар - итоговая работа
+# https://github.com/oberon722/repo_my_hworks_py.git
+#
+# 2. Устанавливаем консольный Git на комп с ресурса:
+# https://git-scm.com
+#
+# 3. Отличная текстовая инструкция по Git:
+# https://github.com/arletino/git_instructions/blob/main/pull_request.md
+#
+# 4. Отличная видеоинструкция по установке Git:
+# https://www.youtube.com/watch?v=jX-js9-UdRs
+# Важно!!!!
+# При этом кода в ветке main у вас быть не должно!!!!!!!!!!!!!!
+#
+# 5. Каталог установки Git по умолчанию!!!!!
+# C:\Program Files\Git
 
 
 print("ЭТО ПРОВЕРКА GITA")
